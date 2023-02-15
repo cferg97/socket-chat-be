38b0f6a8d294846eaf3a1474beaf0ff199a6514d
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-02-15 13:41:29 +0000

    well it works I guess

---
+++ src/socket/index.js
@@ -0,0 +1,27 @@
+let onlineUsers = [];
+
+export const newConnectionHandler = (newClient) => {
+  console.log("New Connection:", newClient.id);
+
+  newClient.emit("welcome", { message: `Hello ${newClient.id}` });
+
+  newClient.on("setUsername", (payload) => {
+    console.log(payload);
+
+    onlineUsers.push({ username: payload.username, socketId: newClient.id });
+
+    newClient.emit("loggedIn", onlineUsers);
+
+    newClient.broadcast.emit("updateOnlineUsersList", onlineUsers);
+  });
+
+  newClient.on("sendMessage", (message) => {
+    console.log("New message:", message);
+    newClient.broadcast.emit("newMessage", message);
+  });
+
+  newClient.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== newClient.id);
+    newClient.broadcast.emit("updatedOnlineUsersList", onlineUsers);
+  });
+};
